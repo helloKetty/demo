eff6b57a8d14721fbf7d41698118a4e07c8240d6
谢亮亮 <xieliangliang@rocoinfo.com>
2022-03-28 13:56:34 +0800

    add notes about Stage 2 - advanced requirement

--- src/main/java/com/example/demo/dedup/StringDeduplicate.java
+++ src/main/java/com/example/demo/dedup/StringDeduplicate.java
@@ -62,7 +62,7 @@ public class StringDeduplicate {
     /**
      * deduplication
      * For a given string that only contains alphabet characters a-z, if 3 or more consecutive
-     * characters are identical, remove them from the string. Repeat this process until
+     * characters are identical,  replace them with a single character that comes before it alphabetically. Repeat this process until
      * there is no more than 3 identical characters sitting besides each other.
      *
      * @param  input
